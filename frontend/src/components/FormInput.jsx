b41fdc4b9379c8ac3697e3dbbffd5acac3f990bb
divverma2003 <divyamalikverma@gmail.com>
2025-09-05 12:16:46 -0400

    Created registration form. Updated background gradient z-index to prevent mouse-clicking conflicts.

--- frontend/src/components/FormInput.jsx
+++ frontend/src/components/FormInput.jsx
@@ -1,8 +1,22 @@
 import React from "react";
-import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
+import {
+  UserPlus,
+  Mail,
+  Lock,
+  LockOpen,
+  User,
+  ArrowRight,
+  Loader,
+} from "lucide-react";
 
-// Label, name, type, required, placeholder, icon
+// Label, name, type, required, placeholder, icon, value
 const FormInput = (props) => {
+  // Handler to call parent's onChange with just the new value (callback)
+  const handleChange = (event) => {
+    props.onChange(event.target.value);
+    // console.log(event.target.value);
+  };
+
   return (
     <div>
       <label
@@ -21,12 +35,11 @@ const FormInput = (props) => {
         </div>
         <input
           id={props.name}
+          name={props.name}
           type={props.type}
           required={props.required}
-          value={formData.name}
-          onChange={(event) =>
-            setFormData({ ...formData, name: event.target.value })
-          }
+          value={props.value}
+          onChange={(event) => handleChange(event)}
           className="block w-full px-3 py-2 pl-10 bg-gray-700 border border-gray-600 rounded-md shadow-sm
 									 placeholder-gray-400 focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm"
           placeholder={props.placeholder}
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { UserPlus, Mail, Lock, User, ArrowRight, Loader } from "lucide-react";
+import {
+  UserPlus,
+  Mail,
+  Lock,
+  LockOpen,
+  User,
+  ArrowRight,
+  Loader,
+} from "lucide-react";
 import { motion } from "framer-motion";
+import { set } from "mongoose";
+import FormInput from "../components/FormInput";
 
 const RegisterPage = () => {
   // Placeholder state
   const loading = true;
   const [formData, setFormData] = useState({
-    username: "",
+    name: "",
     email: "",
     password: "",
     confirmPassword: "",
@@ -41,35 +51,89 @@ const RegisterPage = () => {
         {/* space-y-6 for spacing between children */}
         <div className="bg-gray-800 py-8 px-4 shadow sm:rounded-lg sm:px-10">
           <form onSubmit={handleSubmit} className="space-y-6">
-            <div>
-              <label
-                htmlFor="name"
-                className="block text-sm font-medium text-gray-300"
-              >
-                Full name
-              </label>
-              <div className="mt-1 relative rounded-md shadow-sm">
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <User className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                </div>
-                <input
-                  id="name"
-                  type="text"
-                  required
-                  value={formData.name}
-                  onChange={(event) =>
-                    setFormData({ ...formData, name: event.target.value })
-                  }
-                  className="block w-full px-3 py-2 pl-10 bg-gray-700 border border-gray-600 rounded-md shadow-sm
-									 placeholder-gray-400 focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm"
-                  placeholder="John Doe"
-                />
-              </div>
-            </div>
+            <FormInput
+              label="Full Name"
+              name="name"
+              type="text"
+              required
+              placeholder="John Doe"
+              icon={User}
+              value={formData.name}
+              // parent props method to update state - receives just the value
+              onChange={(value) => setFormData({ ...formData, name: value })}
+            />
+            <FormInput
+              label="Email Address"
+              name="email"
+              type="text"
+              required
+              placeholder="johnny@example.com"
+              icon={Mail}
+              value={formData.email}
+              // parent props method to update state - receives just the value
+              onChange={(value) => setFormData({ ...formData, email: value })}
+            />
+            <FormInput
+              label="Password"
+              name="password"
+              type="password"
+              required
+              placeholder="************"
+              icon={LockOpen}
+              value={formData.password}
+              // parent props method to update state - receives just the value
+              onChange={(value) =>
+                setFormData({ ...formData, password: value })
+              }
+            />
+            <FormInput
+              label="Confirm Password"
+              name="confirmPassword"
+              type="password"
+              required
+              placeholder="************"
+              icon={Lock}
+              value={formData.confirmPassword}
+              // parent props method to update state - receives just the value
+              onChange={(value) =>
+                setFormData({ ...formData, confirmPassword: value })
+              }
+            />
+            <button
+              type="submit"
+              className="w-full flex justify-center py-2 px-4 border border-transparent 
+							rounded-md shadow-sm text-sm font-medium text-white bg-emerald-600
+							 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2
+							  focus:ring-emerald-500 transition duration-150 ease-in-out disabled:opacity-50"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Loader
+                    className="mr-2 h-5 w-5 animate-spin"
+                    aria-hidden="true"
+                  />
+                  Loading...
+                </>
+              ) : (
+                <>
+                  <UserPlus className="mr-2 h-5 w-5" aria-hidden="true" />
+                  Register
+                </>
+              )}
+            </button>
           </form>
+          <p className="mt-8 text-center text-sm text-gray-400">
+            Have an Account?{" "}
+            <Link
+              to="/login"
+              className="font-medium text-emerald-400 hover:text-emerald-300"
+            >
+              Login Instead <ArrowRight className="inline h-4 w-4" />
+            </Link>
+          </p>
         </div>
       </motion.div>
-      RegisterPage
     </div>
   );
 };
